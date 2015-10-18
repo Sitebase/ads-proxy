156a68aa461c35edaa2ed3809a33bc6a306bf9ad
Wim <wim@sitebase.be>
2015-10-18 12:01:21 +0200

    use dynamic name

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function sendUpdate(name, value)
     name = name.replace('.', '');
 
     console.log('SEND', name, value);
-    request('http://192.168.1.117:8080/CMD?LivingLight=' + value, function (error, response, body) {
+    request('http://192.168.1.117:8080/CMD?' + name + '=' + value, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             //console.log(body) // Show the HTML for the Google homepage.
         }
