2ae07a2b3d8048eff055aaa968cab07be692c2df
Wim <wim@sitebase.be>
2016-08-23 19:24:09 +0200

    add ads stuff again

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
 });
 
+
+var options = {
+    host: "192.168.1.126",
+    amsNetIdTarget: "5.27.137.214.1.1",
+    amsNetIdSource: "192.168.1.116.1.1",
+    amsPortTarget: 851
+};
+
+var myHandle = {
+    // symname: '.lLivingSalon',
+    symname: 'House.lKitchenSink',
+    bytelength: ads.BOOL,
+
+    //OPTIONAL: (These are set by default)
+    //transmissionMode: ads.NOTIFY.ONCHANGE, (other option is ads.NOTIFY.CYLCIC)
+    //maxDelay: 0,  -> Latest time (in ms) after which the event has finished
+    //cycleTime: 10 -> Time (in ms) after which the PLC server checks whether the variable has changed
+};
+
+client = ads.connect(options, function() {
+    this.notify(myHandle);
+});
+
+client.on('notification', function(handle){
+    console.log(handle.value);
+});
+
 process.on('exit', function () {
     console.log("exit");
 });
