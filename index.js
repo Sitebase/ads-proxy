1f4e8785df7d34d5d5a979a3d87f0dda4fb189c9
Wim <wim@sitebase.be>
2016-08-23 19:41:40 +0200

    change output

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
-    response.send('useless page to run this on dokku :)');
+    response.send('useless test page to run this on dokku :)');
 });
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
