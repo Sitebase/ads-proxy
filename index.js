a529b47f64f42aad5e71fb7d38ed053f3a1a0351
Wim <wim@sitebase.be>
2016-08-23 19:18:07 +0200

    remove ads connect

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 var ads = require('ads');
 var express = require('express')
 
-var options = {
-    host: "192.168.1.126",
-    amsNetIdTarget: "5.27.137.214.1.1",
-    amsNetIdSource: "192.168.1.111.1.1",
-    amsPortTarget: 851
-};
-
-var myHandle = {
-    // symname: '.lLivingSalon',
-    symname: 'House.lKitchenSink',
-    bytelength: ads.BOOL,
-
-    //OPTIONAL: (These are set by default)
-    //transmissionMode: ads.NOTIFY.ONCHANGE, (other option is ads.NOTIFY.CYLCIC)
-    //maxDelay: 0,  -> Latest time (in ms) after which the event has finished
-    //cycleTime: 10 -> Time (in ms) after which the PLC server checks whether the variable has changed
-};
-
-client = ads.connect(options, function() {
-    this.notify(myHandle);
-});
-
-client.on('notification', function(handle){
-    console.log(handle.value);
-});
-
 var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
